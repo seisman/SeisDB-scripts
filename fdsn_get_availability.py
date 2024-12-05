92d16fe3ef86866e43ce0fb28c7d64b1ab5bd146
Dongdong Tian <seisman.info@gmail.com>
2024-12-05 17:48:22 +0800

    Improve comments in fdsn_get_availability.py

--- fdsn_get_availability.py
+++ fdsn_get_availability.py
@@ -1,16 +1,18 @@
 """
 Get the data availability of seismic stations from FDSN data centers.
 
-A seismic network or station may start in 1960 but only data since 2000 are
-available from FDSN seismic data centers. This script can get the data
-availability of seismic stations from FDSN data centers.
+A seismic network or station may start in 1960 but only data since 2000 are available
+from FDSN seismic data centers. This script can get the data availability of seismic
+stations from FDSN data centers.
 
 Also see a related ObsPy feature request: https://github.com/obspy/obspy/pull/3002
 
-Author: Dongdong Tian @ CUG
-Inital Date: 2023/10/11
-Repository: https://github.com/seisman/SeisDB-scripts/
+- Author: Dongdong Tian @ CUG
+- Repository: https://github.com/seisman/SeisDB-scripts/
+- History:
+  - 2023/10/11 Initial version.
 """
+
 import sys
 from io import StringIO
 
@@ -25,9 +27,9 @@ def usage():
     print(
         "Get the data availability of seismic stations from FDSN data centers.\n\n"
         f"Usage:\n    python {sys.argv[0]} network station\n\n"
-        "    Station can be a single station name, a comma-separated list of \n"
-        "    station names or a wildcard expression. For a wildcard expression,\n"
-        "    enclose it in quotes.\n\n"
+        "    'network'/'station' can be a single name, a comma-separated list of \n"
+        "    names or a wildcard expression. For a wildcard expression, enclose it \n"
+        "    in quotes.\n\n"
         f"Example:\n    python {sys.argv[0]} IM 'TX*'\n"
         ""
     )
@@ -50,7 +52,7 @@ if r.status_code != 200:
 df = pd.read_csv(
     StringIO(r.text),
     names=["network", "station", "location", "channel", "starttime", "endtime"],
-    delim_whitespace=True,
+    sep=r"\s+",
 )
 df["starttime"] = pd.to_datetime(df["starttime"])
 df["endtime"] = pd.to_datetime(df["endtime"])
